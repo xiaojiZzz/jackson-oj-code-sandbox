31f5e9328928f72f3c93be6460f66ae9750247f7
jijunwei <56964585@qq.com>
2024-08-10 13:19:58 +0800

    未完待续

--- src/main/java/com/jackson/ojcodesandbox/codesandbox/java/JavaDockerAcmCodeSandbox.java
+++ src/main/java/com/jackson/ojcodesandbox/codesandbox/java/JavaDockerAcmCodeSandbox.java
@@ -2,21 +2,24 @@ package com.jackson.ojcodesandbox.codesandbox.java;
 
 import com.jackson.ojcodesandbox.model.dto.ExecuteMessage;
 import com.jackson.ojcodesandbox.template.JavaCodeSandboxTemplate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.List;
 
 /**
- * Java Docker 代码沙箱，ACM 形式
+ * Cpp Docker 代码沙箱，ACM 形式
  *
  * @author jackson
  */
 @Component
+@Slf4j
 public class JavaDockerAcmCodeSandbox extends JavaCodeSandboxTemplate {
-
     @Override
     protected List<ExecuteMessage> runCode(String dir, List<String> inputList) throws IOException {
+
+        // 未完待续
         return null;
     }
 }
